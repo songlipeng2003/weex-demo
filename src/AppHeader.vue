c75028127bfecb00b1236e3e2b5df97e10fd5d88
Thinking Song <123wuming_123@163.com>
2017-06-22 17:14:55 +0800

    Add header

---
+++ src/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <text class="title">{{title}}</text>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+    height: 20px;
+  }
+
+  .title {
+    font-size: 18px;
+    text-align: center;
+  }
+</style>
+
+<script>
+  export default {
+    props: [
+      'title'
+    ]
+  }
+</script>
--- src/index.vue
+++ src/index.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="wrapper" @click="update">
-    <image :src="logoUrl" class="logo"></image>
-    <text class="title">Hello {{target}}</text>
-    <text class="desc">Now, let's use vue to build your weex app.</text>
+  <div>
+    <app-header title="首页"></app-header>
+    <div>
+    </div>
   </div>
 </template>
 
 <style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
-  .logo { width: 360px; height: 156px; }
-  .desc { padding-top: 20px; color:#888; font-size: 24px;}
 </style>
 
 <script>
+  import AppHeader from './AppHeader.vue'
+  var modal = weex.requireModule('modal')
+
   export default {
+    components: { AppHeader },
     data: {
-      logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
-      target: 'World'
+      title: 'Demo'
+    },
+    ready: () => {
+      modal.toast({
+        message: '启动APP',
+        duration: 3
+      })
     },
     methods: {
-      update: function (e) {
-        this.target = 'Weex'
-        console.log('target:', this.target)
-      }
     }
   }
 </script>
