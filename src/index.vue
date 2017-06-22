0fd894e93bbe585b02882831b39bbbb0152e16c7
Thinking Song <123wuming_123@163.com>
2017-06-22 17:38:57 +0800

    Add tabbar

--- src/index.vue
+++ src/index.vue
@@ -3,6 +3,8 @@
     <app-header title="首页"></app-header>
     <div>
     </div>
+
+    <wxc-tabbar :tab-items="tabItems"></wxc-tabbar>
   </div>
 </template>
 
@@ -11,12 +13,45 @@
 
 <script>
   import AppHeader from './AppHeader.vue'
+  require('weex-components')
+
   var modal = weex.requireModule('modal')
 
   export default {
     components: { AppHeader },
     data: {
-      title: 'Demo'
+      tabItems: [
+        {
+          index: 0,
+          title: "图片",
+          titleColor: 0x0,
+          icon: "",
+          image: "http://gtms01.alicdn.com/tps/i1/TB1qw.hMpXXXXagXXXX9t7RGVXX-46-46.png",
+          selectedImage: "http://gtms04.alicdn.com/tps/i4/TB16jjPMpXXXXazXVXX9t7RGVXX-46-46.png",
+          src: "modules/main.js",
+          visibility: "visible"
+        },
+        {
+          index: 1,
+          title: "阅读",
+          titleColor: 0x0,
+          icon: "",
+          image: "http://gtms03.alicdn.com/tps/i3/TB1LEn9MpXXXXaUXpXX9t7RGVXX-46-46.png",
+          selectedImage: "http://gtms02.alicdn.com/tps/i2/TB1qysbMpXXXXcnXXXX9t7RGVXX-46-46.png",
+          src: "modules/read.js",
+          visibility: "hidden"
+        },
+        {
+          index: 2,
+          title: "电影",
+          titleColor: 0x0,
+          icon: "",
+          image: "http://gtms01.alicdn.com/tps/i1/TB1B0v5MpXXXXcvXpXX9t7RGVXX-46-46.png",
+          selectedImage: "http://gtms04.alicdn.com/tps/i4/TB1NxY5MpXXXXcrXpXX9t7RGVXX-46-46.png",
+          src: "modules/movie.js",
+          visibility: "hidden"
+        }
+      ]
     },
     ready: () => {
       modal.toast({
